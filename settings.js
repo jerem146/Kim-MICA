1c9909ebefe88bba4652eb62af2afedf516bf5c7
jerem146 <vasquezever146@gmail.com>
2025-07-19 16:19:36 -0500

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -61,7 +61,7 @@ global.etiqueta = '💖 𝒢𝑅𝐼𝒮-𝑀𝒾𝒸𝒶 💖'
 global.moneda = 'peso Argentino'
 global.welcom1 = '❍ Edita Con El Comando setwelcome'
 global.welcom2 = '❍ Edita Con El Comando setbye'
-global.banner = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742678744381.jpeg'
+global.banner = 'https://files.catbox.moe/5nmy7i.jpg'
 global.avatar = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742678797993.jpeg'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
