f02266c7b6e52dd3ceb2aeebae66f9bba8293008
jerem146 <vasquezever146@gmail.com>
2025-07-19 18:38:52 -0500

    Actualizar grupo-setwelcome.js

--- plugins/grupo-setwelcome.js
+++ plugins/grupo-setwelcome.js
@@ -1,15 +1,16 @@
 let handler = async (m, { conn, text, isRowner }) => {
-  if (!text) return m.reply(`${emoji} Por favor, proporciona una bienvenida para el bot.\n> Ejemplo: #setwelcome Hola user`);
+  if (!text) return m.reply(`${emoji} Por favor, proporciona una bienvenida personalizada.\n> Ejemplo: #setwelcome Hola @user, bienvenido al grupo ✨`);
 
-  global.welcom1 = text.trim();
-  
-  m.reply(`${emoji} La bienvenida del bot ha sido cambiado a: ${global.welcom1}`);
-};
+  let chat = global.db.data.chats[m.chat]
+  chat.welcomeMessage = text.trim()
 
-handler.help = ['setwelcome'];
-handler.tags = ['tools'];
-handler.command = ['setwelcome'];
-handler.owner = false;
-handler.admin = true;
+  m.reply(`${emoji} La nueva bienvenida del grupo ha sido establecida:\n\n📝 ${chat.welcomeMessage}`)
+}
+
+handler.help = ['setwelcome']
+handler.tags = ['tools']
+handler.command = ['setwelcome']
+handler.owner = false
+handler.admin = true
 
 export default handler;
