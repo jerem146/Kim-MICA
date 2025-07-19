caf2dce8f21a8c20b9ebb1b71ec78b7a07072b0a
jerem146 <vasquezever146@gmail.com>
2025-07-19 17:31:16 -0500

    Actualizar descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -43,7 +43,7 @@ handler.help = ['facebook', 'fb']
 handler.tags = ['descargas']
 handler.command = ['facebook', 'fb']
 handler.group = true;
-handler.register = true;
-handler.coin = 2;
+handler.register = false;
+handler.coin = 0;
 
 export default handler
