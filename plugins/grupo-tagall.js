ffd6c4bbd562737e7ea52201ea6eb79d84713345
jerem146 <vasquezever146@gmail.com>
2025-07-19 18:21:33 -0500

    Actualizar grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -3,37 +3,29 @@
 - etiqueta en un grupo a todos
 - https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y
 */
-const handler = async (m, { isOwner, isAdmin, conn, args, participants, usedPrefix }) => {
-  if (usedPrefix === 'a' || usedPrefix === 'A') return;
+const handler = async (m, { isOwner, isAdmin, conn, text, participants, args, command, usedPrefix }) => {
+if (usedPrefix == 'a' || usedPrefix == 'A') return;
 
-  const customEmoji = global.db.data.chats[m.chat]?.customEmoji || '🍫';
-  m.react(customEmoji);
+const customEmoji = global.db.data.chats[m.chat]?.customEmoji || '🍫';
+m.react(customEmoji);
 
-  if (!(isAdmin || isOwner)) {
-    global.dfail('admin', m, conn);
-    throw false;
-  }
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn);
+throw false;
+}
 
-  const mensaje = args.join(' ') || '¡Has sido invocado por los dioses del grupo!';
-  const textoExtra = `╰⸼ ┄ ┄ ┄ ─ ꒰ ୭ *${global.vs || 'Versión'}* ୧ ꒱ ┄ ─ ┄ ⸼`;
+const pesan = args.join ;
+const oi = » INFO : ${pesan};
+let teks = !  MENCION GENERAL  !\n  PARA ${participants.length} MIEMBROS 🗣️\n\n ${oi}\n\n╭  ┄ 𝅄 ۪꒰ \⡞᪲=͟͟͞${botname} ≼᳞ׄ ꒱ ۟ 𝅄 ┄\n;   for (const mem of participants) {   teks += ┊${customEmoji} @${mem.id.split('@')[0]}\n;   }   teks += ╰⸼ ┄ ┄ ┄ ─  ꒰  ׅ୭ ${vs} ୧ ׅ ꒱  ┄  ─ ┄ ⸼;
 
-  const mentions = participants.map(p => p.id);
-  let etiquetas = '';
-  for (const u of participants) {
-    etiquetas += `@${u.id.split('@')[0]} `;
-  }
-
-  await conn.sendMessage(m.chat, {
-    image: { url: 'https://files.catbox.moe/5nmy7i.jpg' },
-    caption: `*「 CORTANA 2.0 TE HA INVOCADO 」*\n\n${mensaje}\n\n${etiquetas}\n\n${textoExtra}`,
-    mentions
-  });
+conn.sendMessage(m.chat, { text: teks, mentions: participants.map((a) => a.id) });
 };
 
-handler.help = ['invocar *<mensaje opcional>*'];
+handler.help = ['todos <mensaje opcional>'];
 handler.tags = ['group'];
-handler.command = ['invocar', 'todos', 'tagall'];
+handler.command = ['todos', 'invocar', 'tagall']
 handler.admin = true;
 handler.group = true;
 
 export default handler;
+
